113ee115b4d84e7a728b96dc520fcb7799410ded
ayush3298 <abairagi311@gmail.com>
2020-11-21 13:25:20 +0530

    Updated static files

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -15,18 +15,25 @@ Including another URLconf
 """
 
 from django.conf import settings
+from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
+from django.views.static import serve
 
 urlpatterns = [
     path('', include('home.urls')),
     path('shop/', include('shop.urls')),
     path('account/', include('users.urls')),
     path('admin/', admin.site.urls),
+    url(r'^static/(?P<path>.*)$', serve, {
+        'document_root': settings.STATIC_ROOT,
+    }),
+
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-else:
-    urlpatterns += staticfiles_urlpatterns()
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#     # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# else:
+#     urlpatterns += staticfiles_urlpatterns()
